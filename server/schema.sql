e1b678add245a93362503947592634e5b4b2671f
MKS-Student <mks.student@hackreactor.com>
2015-03-27 17:29:38 -0700

    Create chat schema. Contains message table, rooms table, and users table.

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,97 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+
   /* Describe your table here.*/
+
+
+
+
+
+
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'Rooms'
+--
+-- ---
+
+DROP TABLE IF EXISTS `Rooms`;
+
+CREATE TABLE `Rooms` (
+  `id` INTEGER(50) NOT NULL AUTO_INCREMENT DEFAULT NULL,
+  `room name` VARCHAR(20) NOT NULL DEFAULT 'NULL',
+  PRIMARY KEY (`id`)
 );
+
+-- ---
+-- Table 'Messages'
+--
+-- ---
+
+DROP TABLE IF EXISTS `Messages`;
+
+CREATE TABLE `Messages` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT DEFAULT NULL,
+  `id_Users` INTEGER(200) NOT NULL,
+  `id_Rooms` INTEGER(50) NOT NULL,
+  `content` VARCHAR(200) NULL,
+  `timestamp` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'Users'
+--
+-- ---
+
+DROP TABLE IF EXISTS `Users`;
+
+CREATE TABLE `Users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` VARCHAR(10) NOT NULL DEFAULT 'NULL',
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `Messages` ADD FOREIGN KEY (id_Users) REFERENCES `Users` (`id`);
+ALTER TABLE `Messages` ADD FOREIGN KEY (id_Rooms) REFERENCES `Rooms` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `Rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `Rooms` (`id`,`room name`) VALUES
+-- ('','');
+-- INSERT INTO `Messages` (`id`,`id_Users`,`id_Rooms`,`content`,`timestamp`) VALUES
+-- ('','','','','');
+-- INSERT INTO `Users` (`id`,`username`) VALUES
+-- ('','');
+
+
+
+
+
+
+
+
 
 /* Create other tables and define schemas for them here! */
 
